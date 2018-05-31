d844c899412ec55aeb079c248d5ea8d26999fb40
Etienne Fachaux <etienne.fachaux@skyloud.fr>
2018-05-31 10:02:01 +0200

    Test

--- callbacks.js
+++ callbacks.js
@@ -1,27 +1,24 @@
-function resizeImages(source, dest, width, height, callback) {
-    const function readFiles(resolve, reject) {
-        fs.readdir(source, (err, files) => {
-            if(err) {
-                reject(err);
-            }
-            let promises = new Array();
-        files.forEach((filename, fileIndex) => {
-            promises.push(new Promise(resizeImage));
-    })
-    }
-    }
+function readFiles(resolve, reject) {
+	fs.readdir(source, (err, files) => {
+		if(err) {
+			reject(err);
+		}
+		resolve(files);
+	}
+}
 
-    const function resizeImage(resolve, reject) {
-        gm(source + filename)
-            .resize(width, height)
-            .write(dest + "w" + width + "_" + filename,
-                (err, values) => {
-            if (err) {
-                reject(err);
-            } else if (fileIndex == files.length - 1) {
-            resolve(values);
-        }
-    });
-    }
-    new Promise(readFiles).then(files => callback(files));
+function resizeImage(resolve, reject) {
+	gm(source + filename)
+		.resize(width, height)
+		.write(dest + "w" + width + "_" + filename,
+			(err, values) => {
+		if (err) {
+			reject(err);
+		} else if (fileIndex == files.length - 1) {
+		resolve(values);
+	}
+});
+
+function resizeImages(source, dest, width, height) {
+    new Promise(readFiles);
 }
